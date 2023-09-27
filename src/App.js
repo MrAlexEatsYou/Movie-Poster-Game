0d32f034e72a17c5f7b40855690b0cb4e666e3a5
mralexeatsyou <94697592+MrAlexEatsYou@users.noreply.github.com>
2023-09-27 21:57:13 +0000

    Added movie list function

--- src/App.js
+++ src/App.js
@@ -11,8 +11,45 @@ export default function App() {
     gameCurrentLevel: 0,
     levelImgAlt: 0,
     levelImgSrc:
-      "https://images.squarespace-cdn.com/content/v1/5acd17597c93273e08da4786/1547847934765-ZOU5KGSHYT6UVL6O5E5J/Shrek+Poster.png"
+      "https://images.squarespace-cdn.com/content/v1/5acd17597c93273e08da4786/1547847934765-ZOU5KGSHYT6UVL6O5E5J/Shrek+Poster.png",
   });
+
+  function GetMovieList() {
+    function GetMoviePage(pageNum) {
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YWQzZGQ2MzA0OTBmMDcyMDIxNDI1ODU3ZGRmZTUyNyIsInN1YiI6IjY1MTMzZDZhYWFkOWMyMDEzYmQ1Yjc3OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vmrd5wR2GfgxgDjWdo8s07w4WBz8vJOJPkLD11wge_E",
+        },
+      };
+
+      return fetch(
+        `https://api.themoviedb.org/3/movie/top_rated?language=en-US&region=US&page=${pageNum}&with_original_language=en`,
+        options,
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          return response;
+        })
+        .catch((err) => console.error(err));
+    }
+
+    let tmdbPages = 10;
+    let movieList = [];
+
+    for (var page = 1; i <= tmdbPages; page++) {
+      (async function () {
+        moviePage = await GetMoviePage(i);
+        moviePage.results.forEach((el) => {
+          if (el["backdrop_path"]) {
+            movieList.push(el);
+          }
+        });
+      })();
+    }
+  }
 
   function ToggleState(identifiers) {
     let toggledStates = {};
@@ -28,7 +65,7 @@ export default function App() {
     setAppStates((currentState) => {
       return {
         ...currentState,
-        gameCurrentLevel: currentState.gameCurrentLevel + 1
+        gameCurrentLevel: currentState.gameCurrentLevel + 1,
       };
     });
   }
